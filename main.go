fd6ca6065fa1dd4b3f3512cb12c25e8a6f63f6a5
JulianySavazzi <julianysaz@gmail.com>
2026-09-26 00:39:45 -0300

    feat: added new outputs

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 /**
-* Package main provides entry point for the application.
+* Package main provides entrypoint for the application.
 * Package (folders) -> contains many files;
 * Files (in folder) -> contains many functions;
 * Function (in file) -> belongs to a package;
 * For run the application, uses go run filename.go;
-* */
+* Go is a strongly typed programming language;
+* 
+*/
 package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Printf("My Name is %s\n", "Juliany")
+	fmt.Printf("I am %d years old\n", 25)
+	fmt.Printf("I am %f meters tall\n", 1.56)
 }
